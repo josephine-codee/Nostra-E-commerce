d5f2c242f93c8ae94ddc9b6b3bf67ea7b6955a7a
josephine-codee <josephinea02032008@gmail.com>
2025-11-20 22:21:18 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,38 @@
+var productContainer = document.getElementById("products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+
+search.addEventListener("keyup",function()
+{
+    var enteredvalue = event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count=count+1)
+    {
+        var productname = productlist[count].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+
+    }
+})
+
+
+
+var sidenav = document.querySelector(".side-navbar")
+
+function shownavbar()
+{
+   sidenav.style.left="0%"
+}
+function closenavbar()
+{
+   sidenav.style.left="-70%"
+}
+
+
+
